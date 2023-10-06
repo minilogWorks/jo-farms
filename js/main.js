20bc07b10c2fd56979c18158bb65e3437f667c5a
minilog <40396534+minilogWorks@users.noreply.github.com>
2023-10-06 00:47:28 +0000

    First upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+function decreaseNavHeight(event) {
+  var scroll = $(window).scrollTop();
+  console.log(scroll);
+
+  if (scroll > 50) {
+    $(".navbar").css({ "height": "-78px" });
+  } else {
+    $(".navbar").css({ "height": "78" });
+  }
+}
+
+
+$(window).scroll(decreaseNavHeight);
